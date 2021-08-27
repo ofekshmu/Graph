e3e3d93b1a69e0ba1885b9cff2e75010ae5096ba
ofekshmu <ofek.shmuel1@gmail.com>
2021-08-27 19:51:27 +0300

    finshed cuurent graph class, needes debugging

--- Graph.py
+++ Graph.py
@@ -1,12 +1,19 @@
 
+from typing import Optional
 from Edge import Edge
 
 
 class Graph:
     
-    def __Init__(self, graph : dict = {}, vertices : list = [], edges : list = [], Debug : bool = False):
-        self.graph = graph
-        self.debug = False
+    def __Init__(self, vertices : list = [], edges : list = [], debug : bool = False):
+        self.graph = {}
+        
+        for v in vertices:
+            self.addVertice(v)
+        for e in edges:
+            self.addEdge(e, force = True)
+        
+        self.debug = debug
 
     def addEdge(self, e : Edge, force : bool = False):
         """ 
@@ -15,6 +22,7 @@ class Graph:
         @param force - boolean indicating if to force the new edge onto the graph
         even if the corresponding vertices do not exist. vetices will be created and edge
         will be added if @force is True, insertiong will fail otherwise.
+        @returns True upon succesful insertion, False otherwise.
         """
         cond_e = e.end in self.graph.keys()
         cond_s = e.start in self.graph.keys()
@@ -39,9 +47,17 @@ class Graph:
             self.graph[v] = []
         
     
-    def exists(self, value):
+    def exists(self, value : Optional[Edge]):
+        """
+        Receives either a vertices or an Edge and returns True
+        if is present in graph, False otherwise.
+        """
         if value.isinstance(Edge):
-            NotImplemented()
+            end_exists = value.end in self.graph.keys()
+            start_exists = value.start in self.graph.keys()
+            if end_exists and start_exists:
+                if value.end in self.graph[value.start]:
+                    return True
         else:
             if value in self.graph.keys():
                 return True
@@ -51,14 +67,29 @@ class Graph:
         """@returns a list of the vertices in the graph """
         return list(self.graph.values())
 
-    def getEdges(self):
-        NotImplemented()
+    def getVerticesCount(self):
+        """@returns the number of vertices in the graph"""
+        return len(self.graph.keys())
 
     def getNeighboors(self, v):
-        NotImplemented()
+        """
+        @returns the the neighboors of @v.
+        throws an error if @vb does not exist in graph"""
+        if self.exists(v):
+            return self.graph[v]
+        else:
+            print(f"Vertice {v} does not exist.\nNo list returned.")
+            raise RuntimeWarning
+
 
     def setDebugMode(self, state : bool = True):
         self.debug = state
 
     def __repr__(self):
-        NotImplemented()
+        string = "Graph Preview:\n"
+        for v in self.graph.keys():
+            string += f"\t\t{v} --> "
+            for neigh in self.getNeighboors(v):
+                string += f" {neigh},"
+            string = string[:-1]                #cut the last comma
+        return string
