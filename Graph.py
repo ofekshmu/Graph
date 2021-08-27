19a669ad8923144c95d4d38a97ffb184ca1d9b74
ofekshmu <ofek.shmuel1@gmail.com>
2021-08-27 17:43:57 +0300

    basic funtions

--- Graph.py
+++ Graph.py
@@ -1,2 +1,28 @@
+
 class Graph:
-    pass
+    
+    def __Init__(graph : dict = {}, vertices : list = [], edges : list = [], Debug : bool = False):
+        self.graph = graph
+        self.debug = False
+
+    def addEdge(e : tuple):
+        pass
+
+    def addVertice(v):
+        pass
+    
+    def exists():
+        pass
+
+    def getVertices():
+        pass
+
+    def getEdges():
+        pass
+
+    def getNeighboors(v):
+        pass
+
+    def __repr__():
+        """ TODO """
+        return ""
