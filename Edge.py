52557a288f340ac76fcc8eb341e30c0f8e75835e
ofekshmu <ofek.shmuel1@gmail.com>
2021-08-30 22:25:02 +0300

    continued implementations, future changes written in bottom

--- Edge.py
+++ Edge.py
@@ -1,16 +1,23 @@
 class Edge:
-    def __init__(self, s, e, direction :bool = False):
+    def __init__(self, s, e,weight: int = 0, direction :bool = False):
         self.start = s
         self.end = e
         self.direction = direction
-        self.weight = 0
+        self.weight = weight
         self.direction = False
 
     def setWeight(self, w : int):
         self.weight = 0
 
-    def isDir(self):
+    def _isDir(self):
         return self.direction
+    
+    def _flip(self):
+        if self.direction:
+            print("\tWarning!!!\nFlipping an Edge with direction!")
+        temp = self.start
+        self.start = self.end
+        self.end = temp
 
     def __repr__(self):
         connector = "-->" if self.direction else "--"
--- Graph.py
+++ Graph.py
@@ -1,7 +1,6 @@
-from typing_extensions import runtime
 from Edge import Edge
 
-class Graph:
+class Graph(Edge):
     
     def __init__(self, vertices : list = [],
                      edges : list = [], 
@@ -64,17 +63,22 @@ class Graph:
             if end_exists and start_exists:
                 if value.end in self.graph[value.start]:
                     return True
+            #if not value._isDir():
+                #value._flip()
+                #check again for normal edges
         else:
             if value in self.graph.keys():
                 return True
         return False 
 
-    def __shouldAddEdge(self, e :Edge):        
+    def __shouldAdd(self, e :Edge):        
         if isinstance(e, Edge):
-            if e.end == e.start:
-                return True if self.loops else False
-            elif self.exists(e):
-                return True if self.duplicates else False
+            if self.exists(e):
+                if self.duplicates:
+                    if e.end == e.start and not self.loops:
+                        return False
+                    return True
+                return False
             return True
         else:
             self.debuger("__shouldAddEdge","ERORR - something is not right!!")
@@ -116,17 +120,29 @@ class Graph:
             for neigh in self.getNeighboors(v):
                 string += f" {neigh},"
             string = string[:-1] +"\n"                #cut the last comma
+        loopM = "enabled" if self.loops else "disabled"
+        dirM = "enabled" if self.directed else "disabled"
+        dupM = "enabled" if self.duplicates else "disabled"
+        string += f"Loops are {loopM},\nEdge Direction is {dirM},\nDuplicates are {dupM}.\n"
         string +=20*'-'+"\n"
         return string
 
 
 #some tests for the time being:
-g = Graph(debug=True, duplicates=True)
+g = Graph(debug=False, duplicates=True,loops=True)
 print(g)
 g.addVertice(1)
 print(g.exists(1))
-g.addEdge(Edge(1,2))
 g.addEdge(Edge(1,2),force=True)
+g.addEdge(Edge(1,2))
+
+g.addEdge(Edge(5,5),force=True)
+g.addEdge(Edge(5,5),force=True)
+g.addEdge(Edge(7,7),force=True)
+
+#print(g)
+e = Edge(1,2)
+g.addEdge(e,force=True)
 g.addEdge(Edge(1,3),force=True)
 print(g.exists(Edge(1,2)))
 g.addEdge(Edge(5,6),force=True)
@@ -136,5 +152,9 @@ print(g.getNeighboors(5))
 #print(g.getNeighboors(1))
 print(g)
 
-g2 = Graph(vertices=[1,2,5,7,23],edges=[Edge(1,2),Edge(5,7),Edge(10,1),Edge(20,25)])
-print(g2)
+#g2 = Graph(vertices=[1,2,5,7,23],edges=[Edge(1,2),Edge(5,7),Edge(10,1),Edge(20,25)])
+#print(g2)
+
+#implement of non directional double edges
+# implement outstring on empty graph
+# implement check of both sided edges where flipped 
