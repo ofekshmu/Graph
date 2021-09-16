eda9c76f0a8699f4b36c5f5f29150fd0a6cf7896
ofekshmu <ofek.shmuel1@gmail.com>
2021-09-16 23:45:25 +0300

    implemented str for mat function

--- GraphExtensions.py
+++ GraphExtensions.py
@@ -5,6 +5,7 @@ import numpy as np
 class GraphEx(DirectedGraph):
     def __init__(self, g: DirectedGraph):
         self.graph = g
+        self.matrix = []
 
     def getShortestPath():
         shortestPath = []
@@ -20,13 +21,23 @@ class GraphEx(DirectedGraph):
         edge_list = self.graph.getEdges()
         vertices = self.graph.getVertices()
         dim = self.graph.getVerticesCount() + 1
-        matrix = np.zeros((dim,dim), dtype =np.int)
-        matrix[1:,0] = vertices ; self.matrix[0,1:] = vertices
+        self.matrix = np.zeros((dim,dim), dtype =np.int)
+        self.matrix[1:,0] = vertices ; self.matrix[0,1:] = vertices
         res = {vertices[i]: i for i in range(dim - 1)}
         for e in edge_list:
             row = res[e.start]
             col = res[e.end]
-            matrix[row][col] = 1
+            self.matrix[row][col] = 1
             if not e.isDirected():
-                matrix[col][row] = 1
-        return matrix
+                self.matrix[col][row] = 1
+        return self.matrix
+
+    def genMatrixStr(self):
+        str = ""
+        dim = len(self.matrix)
+        for i in range(dim):
+            for j in range(dim):
+                str += f"{self.matrix[i][j]}\t"  
+            str += "\n"
+        return str
+
