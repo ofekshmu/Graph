52b37d572ada694f711b38255b58b91e45933c9f
ofekshmu <ofek.shmuel1@gmail.com>
2021-09-20 21:43:35 +0300

    first commit after rebase, deleted old vertice class

--- GraphExtensions.py
+++ GraphExtensions.py
@@ -61,28 +61,3 @@ class Path(Graph):
         for n in unvisited:
             n.setDistance(current.getDistance(),acc=True)
             if n.getDistance()
-
-
-class v:
-    def __init__(self, name, distance: int = math.inf, visited = False):
-        self.name = name
-        self.visited = visited
-        self.distance = distance
-
-    def visit(self):
-        self.visited = True
-    
-    def isVisited(self):
-        return self.visited
-
-    def setDistnace(self, d: int, acc = False):
-        if acc:
-            self.distance += d
-        else:
-            self.distance = d
-    
-    def getDistance(self):
-        return self.distance
-
-    def getName(self):
-        return self.name
